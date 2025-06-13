db78751f3b14e69c8ca112816f238355f9fe52a6
John Doe <john.doe@gmail.com>
2025-06-13 22:04:48 +0100

    add getting status and 3d model

--- src/Kiriengine/Model3d.php
+++ src/Kiriengine/Model3d.php
@@ -2,15 +2,69 @@
 
 namespace Core45\LaravelKiriengine\Kiriengine;
 
+use Core45\LaravelKiriengine\Exceptions\KiriengineException;
+use Illuminate\Http\Client\Response;
+
 class Model3d extends LaravelKiriengine
 {
-    public function getStatus(string $taskId)
+    /**
+     * Get the status of a 3D model
+     *
+     * @param string $serialize The serial number of the model
+     * @return array
+     * @throws KiriengineException
+     */
+    public function getStatus(string $serialize): array
     {
         $response = $this->makeRequest([
-            'task_id' => $taskId,
+            'serialize' => $serialize,
+            'endpoint' => 'getStatus'
         ]);
 
-        return $response->json();
+        return $this->handleResponse($response);
+    }
+
+    /**
+     * Download a 3D model (zipped)
+     *
+     * @param string $serialize The serial number of the model
+     * @return array
+     * @throws KiriengineException
+     */
+    public function download(string $serialize): array
+    {
+        $response = $this->makeRequest([
+            'serialize' => $serialize,
+            'endpoint' => 'getModelZip'
+        ]);
+
+        return $this->handleResponse($response);
+    }
+
+    /**
+     * Handle API response and throw exceptions if necessary
+     *
+     * @param Response $response
+     * @return array
+     * @throws KiriengineException
+     */
+    protected function handleResponse(Response $response): array
+    {
+        if (!$response->successful()) {
+            throw new KiriengineException(
+                "API request failed: {$response->status()} - {$response->body()}"
+            );
+        }
+
+        $data = $response->json();
+
+        if (!$data['ok']) {
+            throw new KiriengineException(
+                "API error: {$data['msg']} (Code: {$data['code']})"
+            );
+        }
+
+        return $data['data'];
     }
 
     protected function getEndpoint(): string
